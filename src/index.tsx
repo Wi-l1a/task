7f3a345ed42e9f85c1b1b50865ec7f219d70c3d9
Wi-l1a <borzenko605@gmail.com>
2024-09-14 23:32:37 +0600

    add hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter basename="/task">
+  // пришлось использовать HashRouter чтоб не терялась страница в github 
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
